76218470ac9322d09bf37d813be9edc0ef875150
Tom Boone <tbc@tomboone.com>
2025-08-30 19:57:01 -0400

    add blueprints to function_app

---
+++ tvbingefriend_season_service/blueprints/__init__.py
@@ -0,0 +1,14 @@
+"""Blueprints module."""
+from .bp_get_show_seasons import bp as bp_get_show_seasons  # type: ignore
+from .bp_health_monitoring import bp as bp_health_monitoring  # type: ignore
+from .bp_start_get_all import bp as bp_start_get_all  # type: ignore
+from .bp_updates_manual import bp as bp_updates_manual  # type: ignore
+from .bp_updates_timer import bp as bp_updates_timer  # type: ignore
+
+__all__ = [
+    "bp_get_show_seasons",
+    "bp_health_monitoring",
+    "bp_start_get_all",
+    "bp_updates_manual",
+    "bp_updates_timer"
+]
